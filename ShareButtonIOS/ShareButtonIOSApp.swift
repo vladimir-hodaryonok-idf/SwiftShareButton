c0693816b7019da0b9bbf06f566243451265fda8
vladimir <vladimir.hodaryonok>
2022-09-21 20:55:07 +0300

    Initial Commit

---
+++ ShareButtonIOS/ShareButtonIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareButtonIOSApp.swift
+//  ShareButtonIOS
+//
+//  Created by Vladimir on 21.09.22.
+//
+
+import SwiftUI
+
+@main
+struct ShareButtonIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
