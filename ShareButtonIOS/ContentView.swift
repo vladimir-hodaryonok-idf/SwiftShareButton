2c8602e154d7428085f22abe93ecee52e64c83b0
vladimir <vladimir.hodaryonok>
2022-09-23 23:40:53 +0300

    feat: ShareImage

--- ShareButtonIOS/ContentView.swift
+++ ShareButtonIOS/ContentView.swift
@@ -6,30 +6,54 @@
 //
 
 import SwiftUI
-
+import PhotosUI
 
 struct ContentView: View {
-    @State private var message : String = ""
-    func share(){
-        let controller = UIActivityViewController(activityItems:[message], applicationActivities: nil)
-        let scenes = UIApplication.shared.connectedScenes
-        let windowScene = scenes.first as? UIWindowScene
-        let window = windowScene?.windows.first
-        window?.rootViewController?.present(controller, animated: true)
-    }
-
-    var body: some View {
-        VStack {
-            TextField(text: $message){
-            }.border(.secondary)
-            Button("Share",action: share)
-        }
-        .padding()
-    }
+      @State private var message : String = ""
+      @State private var selectedImage : PhotosPickerItem? = nil
+      @State private var selectedImageData: Data? = nil
+      
+      private func shareContent(content : Any){
+            let controller = UIActivityViewController(activityItems:[content], applicationActivities: nil)
+            let scenes = UIApplication.shared.connectedScenes
+            let windowScene = scenes.first as? UIWindowScene
+            let window = windowScene?.windows.first
+            window?.rootViewController?.present(controller, animated: true)
+      }
+      
+      var body: some View {
+            VStack {
+                  TextField(text: $message){
+                  }.border(.secondary)
+                  Button("Share Text",action: {shareContent(content: message)})
+                  PhotosPicker(
+                        selection: $selectedImage,
+                        matching: .images,
+                        photoLibrary: .shared()) {
+                              Text("Select a photo")
+                        }.onChange(of: selectedImage) { newItem in
+                              Task {
+                                    if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                                          selectedImageData = data
+                                    }
+                              }
+                        }
+                  if (selectedImageData.self != nil){
+                        Button(
+                              "Share Image",
+                              action: {shareContent(content: selectedImageData as Any)
+                                    
+                              }
+                        )
+                  }
+                  
+            }
+            .padding()
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+      static var previews: some View {
+            ContentView()
+      }
 }
